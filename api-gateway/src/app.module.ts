ce6bc21e26853b39d9947f37aaddea944c260b74
Mohsen <mohsenmahoski@gmail.com>
2023-03-13 00:35:28 +0330

    Register ClientsModule

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'FIBO_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'app-gateway',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'kafka-microservices',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [],
 })
