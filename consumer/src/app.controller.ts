3b6626517ae05341d37a5d9d39a7a2a9c88b4329
Mohsen <mohsenmahoski@gmail.com>
2023-03-17 19:09:13 +0330

    Consume fibo topics

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  private fibonacci (n: number){
+    return n < 1 ? 0 : n <= 2 ? 1 : this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  };
+
+  @MessagePattern('fibo')
+  getFibonacci(@Payload() message: { num: number }) {
+    const { num } = message;
+    return this.fibonacci(num);
+  }
 }
