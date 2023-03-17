d233057c98d5e3379785593b3dfdc8be4760cf0a
Mohsen <mohsenmahoski@gmail.com>
2023-03-17 18:25:44 +0330

    Instantiate app with createMicroservice of NestFactory

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { v4 as uuidv4 } from 'uuid';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+        AppModule,
+        {
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: `consumer-${uuidv4()}`,
+              brokers: ['host.docker.internal:9092'],
+            },
+            consumer: {
+              groupId: 'consumer',
+            },
+          },
+        },
+      );
+      app.listen();
 }
 bootstrap();
